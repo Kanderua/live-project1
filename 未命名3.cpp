7a42babd1fceb5e15ff1ee8bd57c9bab9c1bd10d
Kanderua <48649997+Kanderua@users.noreply.github.com>
2020-04-08 17:38:23 +0800

    排序

---
+++ 未命名3.cpp
@@ -0,0 +1,19 @@
+void sort()//∞¥—ß∫≈≈≈–Ú
+{
+	int i, j, k, *p, *q, s;
+	char temp[LEN + 1], ctemp;
+	float ftemp;
+	for (i = 0; i<n - 1; i++)
+	{
+		for (j = n - 1; j>i; j--)
+		if (strcmp(stu[j - 1].StudentId, stu[j].StudentId)>0)
+		{
+			strcpy(temp, stu[j - 1].StudentId);
+			strcpy(stu[j - 1].StudentId, stu[j].StudentId);
+			strcpy(stu[j].StudentId, temp);
+			strcpy(temp, stu[j - 1].StudentName);
+			strcpy(stu[j - 1].StudentName, stu[j].StudentName);
+			strcpy(stu[j].StudentName, temp);
+		}
+	}
+}
